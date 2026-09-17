4512b6c1ce49651d42e661b51edf2a4db61809b9
Aditya Malhotra <talk.aditya2011@gmail.com>
2026-09-17 11:20:26 -0700

    i had to do the entire thing again bcz something happened but i asked some questions and did a couple if else loops

--- bin/training_2027_project1.dart
+++ bin/training_2027_project1.dart
@@ -1,5 +1,33 @@
-import 'package:training_2027_project1/main.dart';
+import 'dart:io';
 
 void main(List<String> arguments) {
-  runCli(arguments);
+  print("What is your name?");
+  String name = stdin.readLineSync() ?? 'Error';
+  print("What is your favorite song?");
+  String song = stdin.readLineSync() ?? 'Error';
+  print("Your name is $name and your favorite song is $song");
+  print("Say y if the above statement is correct and n if it is incorrect");
+  String input = stdin.readLineSync() ?? 'n';
+  bool iscorrect = (input == 'y');
+
+  if(iscorrect){
+    print("Nice job! You actually got it correct.");
+    stdout.write("Now pick a random word (and click enter twice after you answer): ");
+
+    String hungry = stdin.readLineSync() ?? 'Error';
+    String tired = stdin.readLineSync() ?? 'Error';
+    String response = stdin.readLineSync() ?? 'Error';
+
+    if(response == hungry || response == tired){
+      print("You seem exhausted huh in one way or another. Well, you passed the questionaire at least so that's one thing done.");
+    }
+    else{
+      print("This was not the response I expected, do this entire thing again but next time in this part say either tired or hungry.");
+    }
+
+  }
+  else {
+    print("Get it right next time. Try Again.");
+  }
+
 }
