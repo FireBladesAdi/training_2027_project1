25a480050a96d50e5a5faae1689536ae233a0fbc
Aditya Malhotra <talk.aditya2011@gmail.com>
2026-09-25 13:07:22 -0700

    Day 7 2nd task mixed with the first one, about classes practice with printing strings and ints, forgot to commit thursday 9/24 so im commiting friday 9/25

--- bin/training_2027_project1.dart
+++ bin/training_2027_project1.dart
@@ -1,33 +1,23 @@
 import 'dart:io';
 
-void main(List<String> arguments) {
-  print("What is your name?");
-  String name = stdin.readLineSync() ?? 'Error';
-  print("What is your favorite song?");
-  String song = stdin.readLineSync() ?? 'Error';
-  print("Your name is $name and your favorite song is $song");
-  print("Say y if the above statement is correct and n if it is incorrect");
-  String input = stdin.readLineSync() ?? 'n';
-  bool iscorrect = (input == 'y');
+class Player {
+  String username;
+  int health;
+  int losthealth;
 
-  if(iscorrect){
-    print("Nice job! You actually got it correct.");
-    stdout.write("Now pick a random word (and click enter twice after you answer): ");
+  Player(this.username, this.health, this.losthealth);
 
-    String hungry = stdin.readLineSync() ?? 'Error';
-    String tired = stdin.readLineSync() ?? 'Error';
-    String response = stdin.readLineSync() ?? 'Error';
-
-    if(response == hungry || response == tired){
-      print("You seem exhausted huh in one way or another. Well, you passed the questionaire at least so that's one thing done.");
-    }
-    else{
-      print("This was not the response I expected, do this entire thing again but next time in this part say either tired or hungry.");
-    }
-
+  int getfinalhealth() {
+    return health - losthealth;
   }
-  else {
-    print("Get it right next time. Try Again.");
-  }
+}
 
+void main(List<String> arguments){
+
+  Player info = Player("RandomDude101", 100, 1);
+
+  print('Username = ${info.username} &' + ' Health = ${info.health} &' + ' Final Health = ${info.getfinalhealth()}');
+  print("Username = ${info.username}");
+  print("Health = ${info.health}");
+  print("Final Health = ${info.getfinalhealth()}");
 }
